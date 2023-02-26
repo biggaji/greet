003de196e4598f593bd71fa99b2361e696773eed
Tobiloba Ajibade <tobiajibade@yandex.com>
2023-02-26 17:08:27 +0100

    added error handler to check for empty or invalid name

--- greet.go
+++ greet.go
@@ -1,9 +1,16 @@
 package greet
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Insert user's name into message and return a string
-func SayHi(name string) string {
+func SayHi(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("name must be provided")
+	}
+
 	message := fmt.Sprintf("Hey %v. Welcome to Go land!", name)
-	return message
+	return message, nil
 }
