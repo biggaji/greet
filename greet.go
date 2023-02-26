45f398843d5f910aa8c9e796ab55c5149870d282
Tobiloba Ajibade <tobiajibade@yandex.com>
2023-02-26 17:44:04 +0100

    added a random message generator to greet.SayHi

--- greet.go
+++ greet.go
@@ -3,14 +3,30 @@ package greet
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 // Insert user's name into message and return a string
 func SayHi(name string) (string, error) {
 	if name == "" {
-		return "", errors.New("name must be provided")
+		return name, errors.New("name must be provided")
 	}
 
-	message := fmt.Sprintf("Hey %v. Welcome to Go land!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomFormat() string {
+	predefinedMessages := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	return predefinedMessages[rand.Intn(len(predefinedMessages))]
 }
